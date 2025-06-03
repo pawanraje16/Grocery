992ec4bb34397dfb3ac2f57f47be96ae579fce15
pawanraje16 <136164835+pawanraje16@users.noreply.github.com>
2025-06-03 17:46:45 +0530

    Add main application structure with routing, context, and components
    - Implemented Navbar, MainBanner, and Categories components.
    - Set up AppContext for global state management.
    - Integrated routing with React Router.
    - Updated App component to conditionally render Navbar based on path.

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainBanner from '../components/MainBanner'
+import Categories from '../components/Categories'
+
+export default function Home() {
+  return (
+    <div className='mt-10'>
+      <MainBanner/>
+      <Categories/>
+    </div>
+  )
+}
