dae179aa3ce56a662bccd5f213057bc0a484837a
pawanraje16 <136164835+pawanraje16@users.noreply.github.com>
2025-06-04 11:59:57 +0530

    Add ProductDetails page and update routing; enhance ProductCard for navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import { useAppContext } from './context/AppContext'
 import Login from './components/Login'
 import AllProducts from './pages/AllProducts'
 import ProductCategory from './pages/ProductCategory'
+import ProductDetails from './pages/ProductDetails'
 
 function App() {
    
@@ -24,6 +25,7 @@ function App() {
           <Route path='/' element={<Home/>}/>
           <Route path='/products' element={<AllProducts/>}/>
           <Route path='/products/:category' element={<ProductCategory/>} />
+          <Route path='/products/:categroy/:id' element={<ProductDetails/>} />
         </Routes>
       </div>
       {!isSellerPath && <Footer/>}
